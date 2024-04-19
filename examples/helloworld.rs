2d64f54f7379175560296573d9ba26e15e045aa6
Nupur Thakur <hinupurthakur@gmail.com>
2024-04-19 23:52:19 +0530

    add hello world code

---
+++ examples/helloworld.rs
@@ -0,0 +1,12 @@
+// To compile the program, run the following command:
+// rustc hello.rs
+
+// To run the program, run the following command:
+// ./helloworld
+
+fn main() {
+    println!("Hello, world!");
+    println!("I'm Nupur Thakur and a Rustacean!");
+}
+
+
